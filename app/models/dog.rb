7aadc01f4b30a896aad47f1690d4b3a6a2f58938
Jeffrey Paulino <jp7kode@gmail.com>
2019-11-07 17:41:31 -0500

    Done.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,24 @@
 class Dog < ApplicationRecord
+  # has_and_belongs_to_many :employees
+ has_many :employees
+
+ validates :name, :breed, :age, presence: true
+ # validates :
+
+  scope :sorted_by_employee, lambda { where(:self.employees).count }
+ # scope :visible, lambda { where(:visible => true) }
+
+  @is_sorted = false
+
+ 
+
+  def to_s
+   self.name + " " + self.breed + " " + self.age.to_s
+  end
+
+ def self.toggle_sort
+  # self.i
+  @is_sorted = !@is_sorted
+ end
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,11 @@
 class Employee < ApplicationRecord
+  belongs_to :dog
+
+  validates :first_name, :last_name, :alias, :title, :office, :dog_id, :img_url, presence: true
+  validates :alias, uniqueness: true
+
+  def to_s
+    self.first_name + " " + self.last_name
+  end
+
 end
