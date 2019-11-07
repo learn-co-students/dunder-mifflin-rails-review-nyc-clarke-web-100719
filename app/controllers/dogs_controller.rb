e44c532fee58b228263e6665959ba0b1dbffdb53
yoshinator <yoanante@gmail.com>
2019-11-07 16:03:56 -0500

    solution

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,22 @@
 class DogsController < ApplicationController
+  before_action :set_dog, only: [:show]
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def sorted 
+    @dogs = Dog.sorted
+  end 
+
+  def show 
+  end 
+
+
+
+  private 
+  def set_dog
+    @dog =Dog.find(params[:id])
+  end 
+
 end
